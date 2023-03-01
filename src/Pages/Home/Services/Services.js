f5432dcc75675111f2c28102a13665e34df6f143
riyad5541 <abdullahalriyad5541@gmail.com>
2023-03-01 15:57:27 +0600

    service polishing

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Loading from "../../Shared/Loading/Loading";
 import ServiceCard from './ServiceCard';
